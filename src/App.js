ce7b93ee48772e7916afde63b0be1f755960a8b3
Nicolas Rodriguez <skyw4.rg@gmail.com>
2023-11-09 22:53:45 -0300

    finalizacion de la primer preentrega

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+
+import "bulma/css/bulma.css";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <section className="hero">
+        <div className="hero-body">
+      <ItemListContainer greeting={'No pierdas la oportunidad y contactate con nosotros!'}/>
+        </div>
+      </section>
+
     </div>
-  );
+  )
 }
+
 
 export default App;
